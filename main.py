9861d39ff5979028d1abeba6211a2cac32705119
Ompio <72317376+Ompio@users.noreply.github.com>
2023-05-28 17:25:07 +0200

    new files

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #from jinja2 import Template
+import logging
+import os
+
 from menage_directories import prepare_files
 
 
 if __name__ == '__main__':
     # Make test data from html
-    prepare_files('test1')
-    print('dupa')
+    logging.basicConfig(format='\033[97m[\033[92m%(levelname)s\033[97m]-%(message)s',level=logging.DEBUG)
+    prepare_files('nazwa_testu')
+    logging.debug('active dir: '+os.getcwd())
